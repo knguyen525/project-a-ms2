9b7b2ada5c446ca6ae9cd6af313be11d704a448d
knguyen525 <khanh@cloudinox.com>
2018-12-16 23:15:50 -0800

    Update index.py

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World from MS2',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
